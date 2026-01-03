3e2c9287fd500447ba64d4fa09474a8343e38264
luckybanda <lbanda@byupathway.edu>
2026-01-03 12:20:31 +0200

    Update Program.cs

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,21 +1,76 @@
-// Referencing the system namespace
 using System;
 
-// Our shell for all the functions
 class Program
 {
-    // Main Function which can recieve parameters
     static void Main(string[] args)
     {
-        // Prompt the user to enter their first name.
-        Console.WriteLine("What is your first name? ");
-        string firstName = Console.ReadLine();
+        // Prompt the user to enter their percentage
+        Console.Write("What is your grade percentage? ");
+        // Change type from string to interger
+        int percentage = int.Parse(Console.ReadLine());
 
-        // Prompt the user to enter their last name.
-        Console.WriteLine("What is your last name? ");
-        string lastName = Console.ReadLine();
+        
+        
 
-        // Prints out the interporated output.
-        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
+        //Identify the grade letter
+        string grade = "";
+
+        // Condition checking
+        if (percentage >= 90)
+        {
+            grade = "A";
+        }
+        else if (percentage >= 80)
+        {
+            grade = "B";
+        }
+        else if (percentage >= 70)
+        {
+            grade = "C";
+        }
+        else if (percentage >= 60)
+        {
+            grade = "D";    
+        }
+        else
+        {
+            grade = "F";
+        }
+
+        // Identify the sign
+        string sign = "";
+        int lastDigit = percentage % 10;
+
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            sign = "-";
+        }
+
+        // Handling special cases
+        //A+ grade not available
+        if (grade == "A" && sign == "+")
+        {
+            sign = "";
+        }
+         // No F+ or F- grades
+        if (grade == "F")
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade is: {grade}{sign}");
+
+        if (percentage >= 70)
+        {
+            Console.WriteLine("You Passed!");
+        }
+        else
+        {
+            Console.WriteLine("Better luck next time!");
+        }
     }
 }
