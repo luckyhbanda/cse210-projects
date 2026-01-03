b7cc6419b7b9b2d251dbb9b298eb22d2f7161558
luckybanda <lbanda@byupathway.edu>
2026-01-03 10:22:23 +0200

    Update Program.cs
    I have added comments to my code.

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,13 @@
+// Referencing the system namespace
 using System;
 
+// Our shell for all the functions
 class Program
 {
+    // Main Function which can recieve parameters
     static void Main(string[] args)
     {
+        // Body of the code
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
     }
 }
