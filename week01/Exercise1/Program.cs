e423320e962385100b40a7de20a2aadf0be9d813
luckybanda <lbanda@byupathway.edu>
2026-01-03 11:44:20 +0200

    Update Program.cs
    Naming convention has changed from snake case to camel case as this is the best practice in C# development.

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -9,13 +9,13 @@ class Program
     {
         // Prompt the user to enter their first name.
         Console.WriteLine("What is your first name? ");
-        string first_name = Console.ReadLine();
+        string firstName = Console.ReadLine();
 
         // Prompt the user to enter their last name.
         Console.WriteLine("What is your last name? ");
-        string last_name = Console.ReadLine();
+        string lastName = Console.ReadLine();
 
         // Prints out the interporated output.
-        Console.WriteLine($"Your name is {last_name}, {first_name} {last_name}.");
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
